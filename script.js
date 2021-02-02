08ecfce2d04fa7b028d67bf2d8276e2fa496e217
Krushil <krushilnaik96@gmail.com>
2021-02-02 15:33:24 -0500

    ask the user for password preferences

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-	var password = generatePassword();
+	passwordLength = Number(window.prompt("How long do you want your password to be?\n(must be at least 8 characters)"));
+
+	if (passwordLength < 8) {
+		alert("Please specify a length greater than 8.");
+		return;
+	}
+
+	useUppercase = confirm("Would you like to use uppercase characters?\n('OK' for yes, 'Cancel' for no)");
+	useLowerCase = confirm("Would you like to use lowercase characters?\n('OK' for yes, 'Cancel' for no)");
+	useNumeric   = confirm("Would you like to use numbers?\n('OK' for yes, 'Cancel' for no)");
+	useSpecial   = confirm("Would you like to use special characters?\n('OK' for yes, 'Cancel' for no)");
+
+	var password = generatePassword(
+		passwordLength,
+		useUppercase,
+		useLowerCase,
+		useNumeric
+	);
+
 	var passwordText = document.querySelector("#password");
 
 	passwordText.value = password;
